f2736a13671898177812c2821ce637c15aada960
Egor Becker <niro0842@gmail.com>
2025-11-29 01:15:13 +0100

    fix: add base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/simulator-dodepa/', // Базовый путь для GitHub Pages
   plugins: [
     vue(),
     vueDevTools(),
